56a51210fe406a822dabdbb6b12c057d6d78156b
adamZdrojewski <adam.zdrojewski2@gmail.com>
2024-10-30 11:01:21 -0400

    Adds support for environment variables and adds a check to ensure required variables are included.

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
+import "dotenv/config";
 import express from "express";
 import testRouter from "./routers/testRouter.js";
 
+// Check for environment variables
+if(!process.env.AMADEUS_API_KEY || !process.env.AMADEUS_API_SECRET) {
+    // Script does not have all required environment variables
+    if(!process.env.AMADEUS_API_KEY) {
+        console.error("Missing required variable AMADEUS_API_KEY");
+    }
+    if(!process.env.AMADEUS_API_SECRET) {
+        console.error("Missing required variable AMADEUS_API_SECRET");
+    }
+    process.exit(1);
+}
+
+// Constants
+const AMADEUS_API_KEY = process.env.AMADEUS_API_KEY
+const AMADEUS_API_SECRET = process.env.AMADEUS_API_SECRET;
 const app = express();
 
+// Express setup
 app.use(express.json());
-
 app.use("/testRouter", testRouter);
 
-app.listen(3000, () => {
+// Listen for requests
+app.listen(3001, () => {
     console.log("Server Started!");
 });
