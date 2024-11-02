3ce841f94b963509c549e9264bf81602130dca92
adamZdrojewski <adam.zdrojewski2@gmail.com>
2024-11-02 10:57:34 -0400

    Adds Hotel wrapper class for Amadeus API.  Also implements Vitest.

---
+++ src/logic/Hotels.js
@@ -0,0 +1,62 @@
+import "dotenv/config";
+import Amadeus from "amadeus";
+
+export default class Hotels {
+    amadeus;
+
+    constructor() {
+        this.amadeus = new Amadeus({
+            clientId: process.env.AMADEUS_API_KEY,
+            clientSecret: process.env.AMADEUS_API_SECRET
+        });
+    }
+
+    /**
+     * Gets hotels around certain coordinates from Amadeus API
+     * @param latitude {number}
+     * @param longitude {number}
+     * @param radius {number}
+     * @returns {Promise} Array of hotel objects
+     */
+    async getHotelsAroundGeocode(latitude, longitude, radius = 5) {
+        // Check if parameters exist
+        if(!latitude || !longitude || !radius) {
+            throw new Error("Incorrect parameters");
+        }
+
+        // Check if parameters are numbers
+        let results = [latitude*100, longitude*100, radius*100];
+        for(let i = 0; i < results.length; i++) {
+            if(isNaN(results[i])) {
+                throw new Error("Parameters must be numbers");
+            }
+        }
+
+        // Make request
+        let res;
+        try {
+            res = await this.amadeus.referenceData.locations.hotels.byGeocode.get({
+                latitude,
+                longitude,
+                radius,
+                radiusUnit: "MILE"
+            });
+        } catch(err) {
+            throw new Error(err.description[0].detail);
+        }
+
+        // Compile output
+        let output = [];
+        for(let i = 0; i < res.data.length; i++) {
+            output.push({
+                id: res.data[i].hotelId,
+                name: res.data[i].name,
+                location: res.data[i].geoCode,
+                distance: res.data[i].distance
+            });
+        }
+
+        // Return output
+        return output;
+    }
+}
---
+++ tests/logic/Hotels.test.js
@@ -0,0 +1,43 @@
+import { describe, expect, it } from "vitest";
+import Hotels from "../../src/logic/Hotels.js";
+
+describe("Creating Hotels object", () => {
+    it("Throws no error when no parameters are passed", () => {
+        expect(() => {
+            new Hotels();
+        }).not.toThrow();
+    });
+});
+
+describe("Running getHotelsAroundGeocode function", () => {
+    let hotels = new Hotels();
+
+    it("Returns correct data from API when correct parameters are passed", async () => {
+        // Get data
+        let data = await hotels.getHotelsAroundGeocode(41.48, -82.68);
+
+        // Check data
+        expect(data.length).toBeGreaterThan(0);
+        expect(data[0]).toHaveProperty("id");
+        expect(data[0]).toHaveProperty("name");
+        expect(data[0]).toHaveProperty("location");
+        expect(data[0]).toHaveProperty("distance");
+    });
+
+    it("Returns error when no hotels are found for location", async () => {
+        expect(hotels.getHotelsAroundGeocode(42.21, -81.15, 1)).rejects.toThrow("Nothing found for the requested criteria");
+    });
+
+    it("Throws error if incorrect parameter is passed", async () => {
+        expect(hotels.getHotelsAroundGeocode()).rejects.toThrow("Incorrect parameters");
+        expect(hotels.getHotelsAroundGeocode(1)).rejects.toThrow("Incorrect parameters");
+        expect(hotels.getHotelsAroundGeocode("a", 1)).rejects.toThrow("Parameters must be numbers");
+        expect(hotels.getHotelsAroundGeocode(1, "a")).rejects.toThrow("Parameters must be numbers");
+        expect(hotels.getHotelsAroundGeocode(1, 1, "a")).rejects.toThrow("Parameters must be numbers");
+    });
+
+    it("Returns error if API fails", async () => {
+        // Cause API to error out with bad geocode
+        expect(hotels.getHotelsAroundGeocode(10000000000000, 10000000000000)).rejects.toThrow();
+    });
+});
