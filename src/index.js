c8ae9aaf6fe3caaa6bc0a0c5a076bcbc665e3eba
adamZdrojewski <adam.zdrojewski2@gmail.com>
2025-02-02 11:53:49 -0500

    Installed and implemented cors package to handle cors errors

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import express from "express";
 import getHotelsRouter from "./routers/getHotelsRouter.js";
+import cors from "cors";
 
 // Constants
 const app = express();
 
 // Express setup
+app.use(cors());
 app.use(express.json());
 app.use("/gethotels", getHotelsRouter);
 
 // Listen for requests
 app.listen(3001, () => {
-    console.log("Server Started!");
+	console.log("Server Started!");
 });
